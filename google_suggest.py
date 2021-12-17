4214181bf2a667edbfdddea9a7eeb80d98d439ae
Pierre Bruat <pbruat33@gmail.com>
2021-12-17 12:09:24 +0100

    Create google_suggest.py

---
+++ google_suggest.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import sitemap_ping
+import urllib
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import json
+import requests
+import time
+
+@st.cache
+
+def convert_df(df):
+	return df.to_csv().encode('utf-8')
+
+
+def gSuggest(kw):
+    googleSuggestURL="https://suggestqueries.google.com/complete/search?hl=fr&client=firefox&q="
+    response = requests.get(googleSuggestURL+kw, headers={'User-agent':'Mozilla/5.0'})
+    result = json.loads(response.content.decode('utf-8'))
+    result = result[1]
+    return result
+
+st.title('Find Suggested queries 🕵')
+
+form = st.form(key='my-form')
+kw = form.text_input('keyword')
+#depth = form.text_input('depth')
+submit = form.form_submit_button('Submit')   
+if submit:
+    result = gSuggest(kw)
+    result = pd.DataFrame(result)
+    st.write(result)
+    csv = convert_df(result)
+    st.download_button(label="Download data as CSV", data=csv, file_name="my_data.csv",mime='text/csv')
+
